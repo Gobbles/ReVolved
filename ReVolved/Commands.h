4c89a5507d3a51c6b4fc6d8ff770fbe780dd21ab
Josh Paul <josh.tpaul@gmail.com>
2013-08-10 21:32:16 -0300

    Added Random for different types

--- ReVolved/Commands.h
+++ ReVolved/Commands.h
@@ -1,6 +1,8 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
 
+#include <SFML\System\Vector2.hpp>
+#include <random>
 enum KeyPress
 {
 	KEY_UP = 0,
@@ -17,5 +19,24 @@ const double PI = 3.14159265;
 
 float RadToDeg(float Rad);
 
+float GetRandomFloat(float fMin, float fMax)
+{
+    return (float)rand() * (fMax - fMin) + fMin;
+}
+
+double GetRandomDouble(double dMin, double dMax)
+{
+    return rand() * (dMax - dMin) + dMin;
+}
+
+Vector2 GetRandomVector2(float xMin, float xMax, float yMin, float yMax)
+{
+    return sf::Vector2f(GetRandomFloat(xMin, xMax), GetRandomFloat(yMin, yMax));
+}
+
+int GetRandomInt(int iMin, int iMax)
+{
+    return rand() % (iMax - iMin) + iMin + 1;
+}
 
 #endif
